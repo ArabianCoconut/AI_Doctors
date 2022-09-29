fd448e4135389512e86831a1da7793662a6fd8b8
ArabianCoconut <16560968+ArabianCoconut@users.noreply.github.com>
2022-09-30 00:27:25 +0530

    API V1.0
    * Final version of the API template.

--- Src/Flask_Api/Flask_API.py
+++ Src/Flask_Api/Flask_API.py
@@ -1,25 +1,29 @@
-# flask  --app Flask_app --debug run --port 7777
+# TO RUN: flask  --app Flask_app --debug run --port 7777
+from re import U
 from tracemalloc import start
 from flask import *
 import json
-web = Flask(__name__)
 
+from sklearn.utils import resample
+web = Flask(__name__)
+data_rec = {}
 def start_app():
-    web.run(host="localhost",debug=True,port=7770)
+    web.run(host="localhost",debug=True,port=7777)
 
 @web.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 @web.route("/data", methods=['Post'])
-def data():
-    data=request.form['name']
-    url=url_for("patient_data", name=data)
+def data():  # DATA CAPTURE insert all form capture data.
+    global data_rec  ## Records form input in a dictionary
+    data_rec = {"name":request.form['name'],"age":request.form['age']}
+    url = url_for('patient_data', name=data_rec['name'])
     return redirect(url)
 
 @web.route('/patient_data/<name>', methods=['GET'])
 def patient_data(name):
-    data_rx={"Patient":f"{escape(name)}","Age":f"{request.form['age']}"}
+    data_rx={"Patient":f"{escape(name)}","Age":f"{escape(data_rec['age'])}"}  ## Calls data from dictionary
     return json.dumps(data_rx)
 
 ## Start the app
